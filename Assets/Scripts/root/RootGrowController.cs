1adb91d68d2ad12faccfe15dfd81917cc42cbdd0
Cola <q8731690@gmail.com>
2023-02-04 15:43:09 +0800

    [Root功能] 調整圖層、小改Root Code

--- Assets/Scripts/root/RootGrowController.cs
+++ Assets/Scripts/root/RootGrowController.cs
@@ -6,19 +6,24 @@ public class RootGrowController : MonoBehaviour
 {
     public float circleRadius = 0.5f;
     public RootHeadController rootHeadController;
-    public GameObject newRootSegment;
+    [SerializeField]
+    GameObject newRootSegment;
+    [SerializeField]
+    Transform spawnParents;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if( (rootHeadController.rootList[rootHeadController.rootList.Count-1].transform.position - transform.position).magnitude > 0.5)
+        if ((rootHeadController.rootList[rootHeadController.rootList.Count - 1].transform.position - transform.position).magnitude > 0.5)
         {
             GameObject newBody = Instantiate(newRootSegment);
+            if (spawnParents != null)
+                newBody.transform.parent = spawnParents;
             newBody.transform.position = transform.position;
             rootHeadController.rootList.Add(newBody);
         }
