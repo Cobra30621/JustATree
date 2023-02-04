115b4e0a629be0a1a0510e18b7a9d8774779fd26
Cola <q8731690@gmail.com>
2023-02-04 17:46:50 +0800

    進度上傳 新增trigger，敵人生成器上傳

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public List<GameObject> enemyPrefabs;
+    [SerializeField]
+    Transform enemyRoot = null;
 
     [System.Serializable]
     public struct WaveData
@@ -28,34 +30,57 @@ public class EnemySpawner : MonoBehaviour
     {
         timer += Time.deltaTime;
 
-        if (timer >= GameManager.Instance.nextWaveTime) //生怪時間到，開始生成敵人
+        //時間到，生成下一波怪
+        if (timer >= GameManager.Instance.nextWaveTime)
         {
             timer = 0;
-            //出下一波怪
+            SpawnWaveEnemy();
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Instantiate(enemyPrefabs[0], Vector3.right * 10, Quaternion.identity);
         }
     }
     public void SpawnWaveEnemy()
     {
-        WaveData nowWaveData = waveDatas[GameManager.Instance.nowWave];
+        WaveData nowWaveData;
+        //超出範圍
+        if (GameManager.Instance.nowWave > waveDatas.Count - 1)
+        {
+            nowWaveData = waveDatas[waveDatas.Count - 1];
+        }
+        else
+            nowWaveData = waveDatas[GameManager.Instance.nowWave];
 
         for (int i = 0; i < nowWaveData.enemySpawnData.Length; i++)
         {
             EnemySpawnData spawnData = nowWaveData.enemySpawnData[i];
-            StartCoroutine(DelaySpawnWaveEnemy(spawnData.spawnPoint.position, spawnData.count, spawnData.enemyType));
+            SpawnEnemies(spawnData);
         }
+        GameManager.Instance.nowWave++;
     }
-    IEnumerator DelaySpawnWaveEnemy(Vector3 spawnPosition, int count, int enemyType, float delay = 0.1f)
+    //IEnumerator DelaySpawnWaveEnemy(Vector3 spawnPosition, int count, int enemyType, float delay = 0.1f)
+    //{
+    //    yield return new WaitForSeconds(delay);
+    //    SpawnEnemies(spawnPosition, count, enemyType);
+    //    yield return null;
+    //}
+    void SpawnEnemies(EnemySpawnData spawnData)
     {
-        yield return new WaitForSeconds(delay);
-        SpawnEnemies(spawnPosition, count, enemyType);
-        yield return null;
-    }
-    void SpawnEnemies(Vector3 spawnPosition, int count, int enemyType)
-    {
+        Vector3 spawnPosition = spawnData.spawnPoint.position;
+        int count = spawnData.count;
+        int enemyType = spawnData.enemyType;
+
+        int dir = spawnPosition.x > 0 ? 1 : -1;  //判斷怪物生成在左邊還是右邊
+
         for (int i = 0; i < count; i++)
         {
-            GameObject enemy = Instantiate(enemyPrefabs[enemyType], spawnPosition, Quaternion.identity);
-            enemy.transform.position = spawnPosition + new Vector3(i * 2, 0, 0);
+            EnemyAI enemy = Instantiate(enemyPrefabs[enemyType], spawnPosition, Quaternion.identity).GetComponent<EnemyAI>();
+            enemy.transform.parent = enemyRoot;
+            enemy.transform.position = spawnPosition + new Vector3(i * dir, 0, 0);
+
+            GameManager.Instance.enemyList.Add(enemy);
         }
     }
 }
